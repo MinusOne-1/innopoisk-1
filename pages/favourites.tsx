30c1894c937bd581298212d3d5f6c186b7a1f16b
alexandrachupkova <a.chupkova@innopolis.university>
2022-07-01 13:27:59 +0300

    add: basic UI

---
+++ pages/favourites.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Favourites() {
+
+    return (<>
+            <h1>Nothing</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+        </>
+    );
+
+}
