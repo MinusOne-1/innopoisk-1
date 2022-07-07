7373f10c94ea253f51a371a0e8f4757874235a18
alexandrachupkova <a.chupkova@innopolis.university>
2022-07-07 20:33:50 +0300

    add: 2 modules for sign in and sign up

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -1,19 +1,12 @@
 import React from "react";
 import Menu from "./menu";
+import Signup from "./signup";
+import Signin from "./signin";
 import styles from "../styles/Registration.module.css";
 export default function Registration(){
     return(
         <div className={styles.body}>
-            <div className = {styles.form}>
-                <h1>Join us!</h1>
-                <p className = {styles.meta}>Email</p>
-                <input type={"email"} alt={"email"}/>
-                <p className = {styles.meta}>Password</p>
-                <input type={"password"} alt={"password"}/>
-                <button>Sign up</button>
-                <p className={styles.question}>Already have account?</p>
-                <a href={"hui"}>Sign in</a>
-            </div>
+           <Signin/>
         </div>
     );
 }
---
+++ pages/signup.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "../styles/Registration.module.css";
+
+
+export default function Signup(){
+    return (
+        <div>
+            <div className = {styles.form}>
+                <h1>Join us!</h1>
+                <p className = {styles.meta}>Email</p>
+                <input type={"email"} alt={"email"}/>
+                <p className = {styles.meta}>Password</p>
+                <input type={"password"} alt={"password"}/>
+                <button>Sign up</button>
+                <p className={styles.question}>Already have account?</p>
+                <a href={"hui"}>Sign in</a>
+            </div>
+        </div>
+    );
+}
