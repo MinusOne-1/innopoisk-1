84447eafd3c2076f40e097e06df73423fae13918
alexandrachupkova <a.chupkova@innopolis.university>
2022-07-08 14:37:41 +0300

    add: search and navigation by pages from API and settings page

---
+++ pages/dataAPI.ts
@@ -0,0 +1,5 @@
+export const API_Key = "a2fbe01f-ee06-46cf-b8ca-9d03e0c6bfb8";
+export const IMAGE_API = "https://kinopoiskapiunofficial.tech/api/v2.2/films/{id}/images?type=POSTER"
+export const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=";
+export const API_URL_SEARCH =
+    "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,20 +6,39 @@ import Link from 'next/link';
 
 import Movie from '../pages/movie'
 import Menu from "../pages/menu";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 
-const API_Key = "af70d4de-1ee0-4425-a428-b726e4cbbe0e";
-const IMAGE_API = "https://kinopoiskapiunofficial.tech/api/v2.2/films/{id}/images?type=POSTER"
-const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=2";
-const API_URL_SEARCH =
-    "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+import {API_Key, API_URL_SEARCH} from "./dataAPI";
+import {API_URL_POPULAR} from "./dataAPI";
+
+type Movie = {
+    nameRu: string;
+    posterUrl: string;
+    rating: string;
+    filmId: number
+}
 
 
 const Home: NextPage = () => {
-    const [ movies, setMovies ] = useState([]);
 
-    useEffect( () => {
-        fetch(API_URL_POPULAR, {
+
+
+    const [count, setNext ] = useState(1);
+    function handleButtonCLickNext(){
+        setNext(previousState => previousState + 1);
+    }
+    function handleButtonCLickBack(){
+        setNext(previousState => previousState - 1);
+    }
+    function updateAPI(){
+        return count.toString();
+    }
+
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [terms, setSearchTerms] = useState('');
+
+     useEffect( () => {
+        fetch(API_URL_POPULAR + updateAPI(), {
             headers: {
                 "Content-Type": "application/json",
                 "X-API-KEY": API_Key,
@@ -27,13 +46,39 @@ const Home: NextPage = () => {
         })
             .then(res => res.json())
             .then((data) => {
-                console.log(data);
                 setMovies(data.films);
             });
 
 
-    }, []);
-  return (
+
+
+    }, [count] );
+
+   const handleOnSubmit = (e) => {
+        e.preventDefault()
+
+        if (terms) {
+            fetch(API_URL_SEARCH + terms, {
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-API-KEY": API_Key,
+                },
+            })
+                .then(res => res.json())
+                .then((data) => {
+                    // console.log(data);
+                    setMovies(data.films);
+                });
+            setSearchTerms("")
+        }
+
+    };
+
+    const handleOnChange = (e) => {
+        setSearchTerms(e.target.value)
+    }
+
+    return (<>
     <div className={styles.container}>
       <Head>
         <title>InnoPoisk</title>
@@ -41,12 +86,38 @@ const Home: NextPage = () => {
         <link rel="icon" href="../public/favicon.ico" />
       </Head>
 
-    <Menu/>
 
+        <header className = {styles.head}>
+            <ul className= {styles.hr}>
+                <li><a href="">Home</a></li>
+                <li><a href="/favourites">Favorites</a></li>
+            </ul>
+            <img src={"../InnoPoisk.svg"} alt = {"InnoPoisk"}/>
+            <div className={styles.right}>
+
+                {/*<input type="text" placeholder="Search..." className={styles.search}/>*/}
+
+                <form onSubmit={handleOnSubmit}>
+                    <input type={"text"}
+                           placeholder={"Search..."}
+                           value={terms}
+                           onChange={handleOnChange}
+                    />
+                </form>
+                <a className={styles.btn} href="/registration"><img src={"../user.png"} alt={""}/></a>
+            </div>
+        </header>
         <div className={styles.movie_container}>
             {movies.length > 0 && movies.map((movie) => <Movie key={movie.filmId} {...movie} />)}
     </div>
+        <div className={styles.container}>
+            <button onClick={handleButtonCLickBack}>back</button>
+            <p>{count}</p>
+            <button onClick={handleButtonCLickNext}>next</button>
+        </div>
     </div>
+
+        </>
   )
 }
 
