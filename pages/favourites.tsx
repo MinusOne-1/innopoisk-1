51bf1386eb6b6556c8386f4e39f263be61b09bc2
Minus One <LAnnaIg03@yandex.ru>
2022-07-08 16:14:00 +0300

    fix: code formatting, ESlint rules

--- pages/favourites.tsx
+++ pages/favourites.tsx
@@ -1,15 +1,15 @@
+import React from "react";
 import Link from "next/link";
 
 export default function Favourites() {
-
-    return (<>
-            <h1>Nothing</h1>
-            <h2>
-                <Link href="/">
-                    <a>Back to home</a>
-                </Link>
-            </h2>
-        </>
-    );
-
+  return (
+    <>
+      <h1>Nothing</h1>
+      <h2>
+        <Link href="/">
+          <p>Back to home</p>
+        </Link>
+      </h2>
+    </>
+  );
 }
